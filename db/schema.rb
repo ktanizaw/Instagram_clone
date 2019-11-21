39848c72ce88fce4cc79d6685278bb1cd69c0e20
tanizawa_kenta <kt10025102@gmail.com>
2019-11-21 12:04:04 +0900

    add:メールアドレスのユニーク制約

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_21_021130) do
+ActiveRecord::Schema.define(version: 2019_11_21_030207) do
 
   create_table "pictures", force: :cascade do |t|
     t.text "content"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2019_11_21_021130) do
     t.string "name"
     t.string "email"
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
