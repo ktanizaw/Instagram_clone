664fba67462a22cbcec4563b781b5a865a43cb31
tanizawa_kenta <kt10025102@gmail.com>
2019-11-22 12:29:21 +0900

    add:投稿確認画面

--- app/helpers/pictures_helper.rb
+++ app/helpers/pictures_helper.rb
@@ -1,7 +1,7 @@
 module PicturesHelper
   def choose_new_or_edit
     if action_name == 'new' || action_name == 'confirm' || action_name == 'create'
-      confirm_picutres_path
+      confirm_pictures_path
     elsif action_name == 'edit'
       picture_path
     end
