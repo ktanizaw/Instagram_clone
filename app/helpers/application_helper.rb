224dd6019334a4bf1e242272e5a2e9da699750e4
tanizawa_kenta <kt10025102@gmail.com>
2019-11-24 23:41:38 +0900

    update:ログイン時の表示・非表示

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,2 @@
 module ApplicationHelper
-  include SessionsHelper
-
-  def logged_in?
-  current_user.present?
-  end
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
 end
--- app/helpers/pictures_helper.rb
+++ app/helpers/pictures_helper.rb
@@ -6,7 +6,4 @@ module PicturesHelper
       picture_path
     end
   end
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
-  end
 end
