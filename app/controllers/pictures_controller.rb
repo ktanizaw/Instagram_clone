70f86f2a984f924bc296eff74082bb699f3370fd
tanizawa_kenta <kt10025102@gmail.com>
2019-11-26 19:07:41 +0900

    update:pictures.view

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -23,9 +23,9 @@ class PicturesController < ApplicationController
     if params[:back]
       render :new
     else
-    picture = current_user.pictures.build(picture_params)
-      if picture.save
-        PictureMailer.picture_mail(picture).deliver
+    @picture = current_user.pictures.build(picture_params)
+      if @picture.save
+        PictureMailer.picture_mail(@picture).deliver
         redirect_to pictures_path, notice: "投稿しました！"
       else
         render 'new'
