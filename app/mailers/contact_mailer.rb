8d7ea90078ff68b8931d2b4c92942ed9557bb4c4
tanizawa_kenta <kt10025102@gmail.com>
2019-11-27 11:18:24 +0900

    update:メイラー機能

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
 
-    mail to: "@users.email", subject: "お問い合わせの確認メール"
+    mail to: "kt10025102@gmail.com", subject: "お問い合わせの確認メール"
 end
 end
